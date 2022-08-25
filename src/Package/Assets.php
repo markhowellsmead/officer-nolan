b7d30e5afeae457c5ad9a5ac7fbfa5d8fc63a50a
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-08-25 23:30:03 +0200

    Enqueu frontend scripts

--- src/Package/Assets.php
+++ src/Package/Assets.php
@@ -23,8 +23,16 @@ class Assets
 
 		$min = !sht_theme()->debug;
 
-		$deps = ['wp-block-library'];
+		$css_deps = ['wp-block-library'];
 
-		wp_enqueue_style('sht-style', get_template_directory_uri() . '/assets/styles/ui' . ($min ? '.min' : '') . '.css', $deps, filemtime(get_template_directory() . '/assets/styles/ui' . ($min ? '.min' : '') . '.css'));
+		wp_enqueue_style('sht-style', get_template_directory_uri() . '/assets/styles/ui' . ($min ? '.min' : '') . '.css', $css_deps, filemtime(get_template_directory() . '/assets/styles/ui' . ($min ? '.min' : '') . '.css'));
+
+		$js_deps = [];
+
+		wp_enqueue_script('sht-script', get_template_directory_uri() . '/assets/scripts/ui' . ($min ? '.min' : '') . '.js', $js_deps, filemtime(get_template_directory() . '/assets/scripts/ui' . ($min ? '.min' : '') . '.js'), true);
+		wp_localize_script('sht-script', 'sht_theme', [
+			'directory_uri' => get_template_directory_uri(),
+			'version' => wp_get_theme()->get('Version')
+		]);
 	}
 }
