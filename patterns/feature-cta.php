64fe9c767cc1e8549b3d2c56eb004ceb4b3f9777
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-08-29 23:51:14 +0200

    Add feature CTA pattern

---
+++ patterns/feature-cta.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Title: Feature CTA
+ * Slug: officer-nolan/feature-cta
+ */
+
+?>
+
+<!-- wp:cover {"dimRatio":80,"focalPoint":{"x":"0.83","y":"0.12"},"minHeight":704,"minHeightUnit":"px","customGradient":"linear-gradient(180deg,rgba(0,0,0,0) 49%,rgb(0,0,0) 100%)","contentPosition":"bottom left","isDark":false,"align":"wide","href":"#"} -->
+<div class="wp-block-cover alignwide is-light has-custom-content-position is-position-bottom-left ek-linked-block" style="min-height:704px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-80 has-background-dim has-background-gradient" style="background:linear-gradient(180deg,rgba(0,0,0,0) 49%,rgb(0,0,0) 100%)"></span>
+	<div class="wp-block-cover__inner-container">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"1.5rem"}},"textColor":"body-background"} -->
+		<div class="wp-block-group has-body-background-color has-text-color">
+			<!-- wp:group {"style":{"spacing":{"blockGap":"1.5rem"}}} -->
+			<div class="wp-block-group">
+				<!-- wp:group {"style":{"spacing":{"blockGap":"0px"}}} -->
+				<div class="wp-block-group">
+					<!-- wp:paragraph {"fontSize":"small"} -->
+					<p class="has-small-font-size">Feature</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:heading {"level":1,"fontSize":"max-60"} -->
+					<h1 class="has-max-60-font-size">Lorem Ipsum</h1>
+					<!-- /wp:heading -->
+				</div>
+				<!-- /wp:group -->
+
+				<!-- wp:paragraph -->
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam expedita officiis repellat impedit commodi voluptate perferendis incidunt hic, ipsum quam earum? Reiciendis labore eius vitae autem neque exercitationem? Obcaecati, totam?</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+</div>
+<!-- /wp:cover -->
