97922bfdad150c96241beeca15a27cc328aac382
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-09-29 19:24:33 +0200

    Fix fancybox implementation

--- .build/assets/scripts/fancybox/index.js
+++ .build/assets/scripts/fancybox/index.js
@@ -1,6 +1,9 @@
 //require('@fancyapps/fancybox');
 import { Fancybox } from '@fancyapps/ui';
-// import '@fancyapps/ui/dist/fancybox.css';
+import '@fancyapps/ui/src/Fancybox/Fancybox.scss';
+
+// Override default behaviour
+Fancybox.defaults.infinite = 0;
 
 Fancybox.bind(
 	"a[href$='.jpg']:not([target]):not([download]), a[href$='.jpeg']:not([target]):not([download]), a[href$='.png']:not([target]):not([download]), a[href$='.gif']:not([target]):not([download]), a[href$='.svg']:not([target]):not([download]), a[href$='.webp']:not([target]):not([download])",
